7126d399d166454f7f58de70f654372c78082157
hughesjl1 <14260161+hughesjl1@users.noreply.github.com>
2022-03-29 20:13:55 +0100

    App data

--- data/data.tsx
+++ data/data.tsx
@@ -30,7 +30,28 @@ export const projects : IProjectSpec[] = [
         valueAdd : "Website improved search and in depth evaluattion of algorithms performance",
         skills : ['React', 'Typescript'],
         type : 'commercial'
+    },
+    {
+        title : "Algorithmss R&D",
+        description : "Researched and implemented a dynamic algorithm for portfolio out performance.",
+        image : "/factory.png",
+        url : 'Internal',
+        responsibilites : 'Researched range of options using Python, Pandas, Jupyter Notebooks and various AI/ML libraries',
+        valueAdd : 'improve basline performance by 3-7x',
+        skills : ['Python', 'Pandas', 'Tensorflow', 'Numpy', 'Jupyter'],
+        type : 'commercial'
+    },
+    {
+        title : "MicroService R&D Deployment",
+        description : "A containerised architecture to enable fast delivery of AI models",
+        image : "/factory.png",
+        url : 'Internal',
+        responsibilites : 'Designed, developed and deployed the containerised micro service arcchitexture',
+        valueAdd : 'Enabled quick delivery of models from research to client ready API\'s',
+        skills : ['Python', 'FastApi', 'Docker', 'Numpy', 'Jupyter'],
+        type : 'commercial'
     }
+
 
 ]
 
